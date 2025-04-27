1f7ec2d6dd7e95f4c129a92249e8d90cb0215e88
paul <gnunoi@163.com>
2025-04-27 17:32:09 +0800

    Update exam.py

--- exam.py
+++ exam.py
@@ -901,7 +901,7 @@ class Review:
             # pass
             # 重新从数据库读取数据以验证更新是否成功
             self.df = pd.read_sql_query(f"SELECT * FROM {self.table_name};", self.db.connect)
-            print(self.df.to_string())
+            # print(self.df.to_string())
 
     def JudgeType(self, question):
         type = 0
